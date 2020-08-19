0c6aae0b7e78d221893dfb6dba51382c4d6514de
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-08-19 21:05:16 +0600

    added Product model

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,13 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor','Indoor'),
+        ('Out Door','Out Door')
+    )
+    name = models.CharField(max_length=150, null=True)
+    price = models.FloatField(null=True)
+    category  = models.CharField(max_length=200,choices=CATEGORY,null=True)
+    description = models.CharField(max_length=150, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
