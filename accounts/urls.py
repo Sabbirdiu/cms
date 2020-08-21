a93d5fe8216df449c427518e09ca384ea82bc857
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-08-21 22:15:26 +0600

    rendering the customer data to template

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     
     path('', views.home),
     path('products/',views.products),
-    path('customer/',views.customer),
+    path('customer/<str:pk_test>/',views.customer),
+    
     
 ]
